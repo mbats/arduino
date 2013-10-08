87c05f1cd54a4561f16b0b5980caeb4f673351b4
Mélanie Bats <melanie.bats@obeo.fr>
2013-10-08 10:12:42 +0200

    Fadelight example

--- examples/fr.obeo.dsl.arduino.examples.fadelight/code/build-cli/Sketch.cpp
+++ examples/fr.obeo.dsl.arduino.examples.fadelight/code/build-cli/Sketch.cpp
@@ -7,15 +7,16 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
- ** While1 = fr.obeo.dsl.arduino.impl.WhileImpl@167a912
+ ** While1 = fr.obeo.dsl.arduino.impl.WhileImpl@1fc3a11
  while (brightness<255)
  {
+ toto
   brightness=(brightness+1);
   analogWrite(11,255-brightness);
   delay(5);
  }
- ** While2 = 
+ ** While2 = fr.obeo.dsl.arduino.impl.SetImpl@4559
  analogWrite(11,255-brightness);
  delay(5);
- ** While3 =
+ ** While3 = fr.obeo.dsl.arduino.impl.LevelImpl@119bc7c
 }
