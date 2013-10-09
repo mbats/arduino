6f9c6a5192175741c7ed06513c1a07caa490f5a0
Mélanie Bats <melanie.bats@obeo.fr>
2013-10-09 10:34:27 +0200

    Fix generation for fade light example

--- examples/fr.obeo.dsl.arduino.examples.fadelight/code/Sketch.ino
+++ examples/fr.obeo.dsl.arduino.examples.fadelight/code/Sketch.ino
@@ -6,16 +6,17 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
- ** While1 = fr.obeo.dsl.arduino.impl.WhileImpl@1fc3a11
  while (brightness<255)
  {
- toto
   brightness=(brightness+1);
   analogWrite(11,255-brightness);
   delay(5);
  }
- ** While2 = fr.obeo.dsl.arduino.impl.SetImpl@4559
- analogWrite(11,255-brightness);
- delay(5);
- ** While3 = fr.obeo.dsl.arduino.impl.LevelImpl@119bc7c
+ while (brightness>0)
+ {
+  brightness=(brightness-1);
+  analogWrite(11,255-brightness);
+  delay(8);
+ }
+ delay(200);
 }
