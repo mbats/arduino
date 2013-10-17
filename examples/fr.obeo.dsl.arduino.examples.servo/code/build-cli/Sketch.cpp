cb09534f76bf63e5ff24b59f9126410cd0db3ccf
Mélanie Bats <melanie.bats@obeo.fr>
2013-10-17 11:26:33 +0200

    Migrate servo example to sirius

--- examples/fr.obeo.dsl.arduino.examples.servo/code/build-cli/Sketch.cpp
+++ examples/fr.obeo.dsl.arduino.examples.servo/code/build-cli/Sketch.cpp
@@ -1,13 +1,13 @@
 #include <Arduino.h>
 #include <Servo.h>
-Servo Microservo;
+Servo Microservo1;
 int RotationsensorV1 = 5;
 
 void setup() {
- Microservo.attach(9);
+ Microservo1.attach(9);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
- Microservo.write(analogRead(RotationsensorV1));
+ Microservo1.write(analogRead(RotationsensorV1));
 }
