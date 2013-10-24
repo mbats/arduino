c79a03d365149519da57f491e54786d2122f78c1
Mélanie Bats <melanie.bats@obeo.fr>
2013-10-24 17:20:30 +0200

    Update the tigger example

--- examples/fr.obeo.dsl.arduino.examples.tigger.all/code/Sketch.ino
+++ examples/fr.obeo.dsl.arduino.examples.tigger.all/code/Sketch.ino
@@ -1,25 +1,30 @@
 #include <Servo.h>
-#include <Servo.h>
 int BlueLED = 13;
 int WhiteLED = 11;
 int RedLED = 12;
+int Fan = 7;
 Servo Microservo1;
+Servo Microservo2;
 int Pushbutton1 = 0;
 int Infraredsensor = 4;
-int iter_0;
+int Soundsensor = 1;
+int iter_2;
+int iter_28;
 
 void setup() {
   pinMode(BlueLED, OUTPUT);  
   pinMode(WhiteLED, OUTPUT);  
   pinMode(RedLED, OUTPUT);  
+  pinMode(Fan, OUTPUT);  
  Microservo1.attach(9);
+ Microservo2.attach(8);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
  if (!digitalRead(Pushbutton1))
  {
-  for (iter_0=0; iter_0 < ( 5 ); ++iter_0 )
+  for (iter_2=0; iter_2 < ( 5 ); ++iter_2 )
   {
    digitalWrite(BlueLED, (!HIGH));
    digitalWrite(WhiteLED, (!LOW));
@@ -44,4 +49,19 @@ void loop() {
   Microservo1.write(0);
   delay(400);
  }
+ if (analogRead(Soundsensor)>128)
+ {
+  digitalWrite(BlueLED, (!HIGH));
+  for (iter_28=0; iter_28 < ( 3 ); ++iter_28 )
+  {
+   Microservo2.write(0);
+   delay(50);
+   digitalWrite(Fan, (!HIGH));
+   delay(1500);
+   digitalWrite(Fan, (!LOW));
+   Microservo2.write(60);
+   delay(1500);
+  }
+ }
+ digitalWrite(BlueLED, (!LOW));
 }
