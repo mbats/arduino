81d3d59475e922ae9bd4c821ee23499a6ca2db71
Mélanie Bats <melanie.bats@obeo.fr>
2013-10-24 14:47:47 +0200

    Add Tigger tail example

---
+++ examples/fr.obeo.dsl.arduino.examples.tigger.tail/code/Sketch.ino
@@ -0,0 +1,19 @@
+#include <Servo.h>
+#include <Servo.h>
+Servo Microservo1;
+int Infraredsensor = 4;
+
+void setup() {
+ Microservo1.attach(9);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+ if (!digitalRead(Infraredsensor))
+ {
+  Microservo1.write(90);
+  delay(400);
+  Microservo1.write(0);
+  delay(400);
+ }
+}
