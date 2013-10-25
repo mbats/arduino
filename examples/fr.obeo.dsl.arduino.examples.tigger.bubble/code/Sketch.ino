84f6768d48871a5f321b07cb3179008e03abd0f1
Mélanie Bats <melanie.bats@obeo.fr>
2013-10-25 12:38:19 +0200

    Fix issue on bubble example

--- examples/fr.obeo.dsl.arduino.examples.tigger.bubble/code/Sketch.ino
+++ examples/fr.obeo.dsl.arduino.examples.tigger.bubble/code/Sketch.ino
@@ -1,10 +1,9 @@
 #include <Servo.h>
-#include <Servo.h>
 int BlueLED = 13;
 int Fan = 7;
 Servo Microservo2;
 int Soundsensor = 1;
-int iter_0;
+int iter_3;
 
 void setup() {
   pinMode(BlueLED, OUTPUT);  
@@ -17,13 +16,13 @@ void loop() {
  if (analogRead(Soundsensor)>128)
  {
   digitalWrite(BlueLED, (!HIGH));
-  for (iter_0=0; iter_0 < ( 3 ); ++iter_0 )
+  for (iter_3=0; iter_3 < ( 3 ); ++iter_3 )
   {
    Microservo2.write(0);
    delay(50);
-   digitalWrite(Fan, (!HIGH));
-   delay(1500);
    digitalWrite(Fan, (!LOW));
+   delay(1500);
+   digitalWrite(Fan, (!HIGH));
    Microservo2.write(60);
    delay(1500);
   }
